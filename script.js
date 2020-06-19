2d4e0a8ab364e4842ed3826b67ccc15fa6343c3c
Kevin Mattappally <kevinmat@outlook.com>
2020-06-18 22:00:40 -0700

    JSON object retrieved from API

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-// empty
+// selecting the element for DOM manipulation
+const southAfrica = document.querySelector("#southAfrica");
+
+// retrieving the data from the API
+url = "https://api.covid19api.com/live/country/united-states/status/confirmed";
+const data = fetch(url, {mode: "cors"});
+data.then(response => response.json()).then(data => console.log(data));
