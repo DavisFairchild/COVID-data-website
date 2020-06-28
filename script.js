f14c189b99c86b66a3f973d13b49586ffe245632
Kevin Mattappally <kevinmat@outlook.com>
2020-06-28 11:35:36 -0700

    Converted to async / await syntax

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
-// retrieving the data from the API
-url = "https://covid19-api.org/api/status";
-const data = fetch(url);
-data.then(function(response) {
-        return response.json();
-    })
-    .then(function(response) {
-        // updating the table using DOM manipulation
+"use strict"
+
+// fetching country data from the API
+async function fetchData() {
+    const response = await fetch("https://covid19-api.org/api/status");
+    const data = await response.json();
+    return data;
+}
+
+// updating the table using DOM manipulation
+fetchData()
+    .then(data => {
         const tableData = document.querySelectorAll('.rowData');
+        console.log(data);
         let rowNumber = 0;
         let temp = 0;
-        tableData.forEach((row) => {
-            row.firstElementChild.textContent = response[rowNumber].last_update;
-            document.getElementById("C"+temp).textContent = response[rowNumber].country;
-            row.lastElementChild.textContent = response[rowNumber].cases;
+        tableData.forEach(row => {
+            row.firstElementChild.textContent = data[rowNumber].last_update;
+            document.getElementById("C"+temp).textContent = data[rowNumber].country;
+            row.lastElementChild.textContent = data[rowNumber].cases;
             rowNumber++;
             temp++;            
         });
