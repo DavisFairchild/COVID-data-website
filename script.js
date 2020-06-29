eccd8bf75c6cda780e3963ba63118feb785fc1bf
Kevin Mattappally <kevinmat@outlook.com>
2020-06-28 21:25:17 -0700

    Utilized loops for DOM manipulation

--- script.js
+++ script.js
@@ -1,36 +1,34 @@
 "use strict"
 
-// fetching country data from the API
-async function fetchStatusData() {
+// fetching COVID-19 data from the API by country
+async function fetchStatus() {
     const response = await fetch("https://covid19-api.org/api/status");
     const data = await response.json();
     return data;
 }
 
 // updating the table using DOM manipulation
-fetchStatusData()
+fetchStatus()
     .then(data => {
         let table = document.createElement("table");
         const numCountries = 50;
-        const numCollumns = 3;
-        let thCountry = document.createElement("th");
-        let thCases = document.createElement("th");
-        let countryHeader = document.createTextNode("Country");
-        let casesHeader = document.createTextNode("Confirmed Cases");
-        thCountry.appendChild(countryHeader);
-        thCases.appendChild(casesHeader);
-        table.appendChild(thCountry);
-        table.appendChild(thCases);
+        const numCategories = 4;
+        const tableHeaders = ["Country", "Confirmed Cases", "Recovered", "Deaths"];
+        const jsonMembers = ["country", "cases", "recovered", "deaths"];
+        for (let i = 0; i < numCategories; i++) {
+            let th = document.createElement("th");
+            let thText = document.createTextNode(tableHeaders[i]);
+            th.appendChild(thText);
+            table.appendChild(th);
+        }
         for (let i = 0; i < numCountries; i++) {
             let tr = document.createElement("tr");
-            let tdCountry = document.createElement("td");
-            let tdCases = document.createElement("td");
-            let countryText = document.createTextNode(data[i].country);
-            let casesText = document.createTextNode(data[i].cases);
-            tdCountry.appendChild(countryText);
-            tdCases.appendChild(casesText);
-            tr.appendChild(tdCountry);
-            tr.appendChild(tdCases);
+            for (let j = 0; j < numCategories; j++) {
+                let td = document.createElement("td");
+                let tdText = document.createTextNode(data[i][jsonMembers[j]]);
+                td.appendChild(tdText);
+                tr.appendChild(td);
+            }
             table.appendChild(tr);
         }
         document.getElementById("countryTableSection").appendChild(table);
